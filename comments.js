d27f8260969c86b6d0a95aec39110090b56080cd
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2023-12-27 03:24:38 +0530

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,1 +1,16 @@
 // Create web server
+// Import the HTTP module
+const http = require('http');
+
+// Configure the HTTP server to respond with "Hello, World!" to all requests
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!\n');
+});
+
+// Listen on port 3000 and IP address 127.0.0.1
+const port = 3000;
+const hostname = '127.0.0.1';
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
